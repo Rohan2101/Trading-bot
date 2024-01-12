d36e328d15ae1a7987eccbc27ea66fede29fb0d1
Rohan2101 <104345714+Rohan2101@users.noreply.github.com>
2024-01-12 18:26:11 +0530

    added files

---
+++ MainMenu.py
@@ -0,0 +1,169 @@
+import WazirxAPI
+import RSI1
+import MACD_OLD
+import BB_UTILS
+
+def main_menu():
+    x = 0
+    print("Welcome to Cypher AI Trading Bot ")
+    print("Press 1 for Generating Signals for your selected crypto ")
+    print("Press 2 for Logging in to Wazirx")
+    print("Press 3 for Placing order")
+    print("Press 4 for Updating the historical price for your selected crypto")
+    print("Press 5 to Get Current Price")
+    print("Press 6 for Running Algo Trade Software and Generate Periodical Signals every Specified Amount of Time")
+    x = int(input("Press 7 for Updating Historical data periodically for the Specified Amount of Time\n"))
+    if x == 1:
+        signals_menu()
+    elif x == 2:
+        WazirxAPI.login()
+        main_menu()
+    elif x == 3:
+        WazirxAPI.login()
+        print("Please input the necessary information in the following order")
+        ticker = input("Input Ticker\n")
+        limit = input("Input \'market\' or \'limit\'\n")
+        buy = input("Input \'buy\' or \'sell\' order\n")
+        amount = input("Input the amount\n")
+        price = input("Input the buy or sell price\n")
+        WazirxAPI.Place_order(ticker, limit, buy, amount, price)
+        main_menu()
+    elif x == 4:
+        get_symbol_data()
+        main_menu()
+    elif x == 5:
+        ticker = input("Input the ticker\n")
+        print(WazirxAPI.get_current_price(ticker))
+        main_menu()
+    elif x ==6:
+        print("Please input the necessary information in the following order")
+        symbol = input("Please input symbol of crypto\n")
+        scan_interval = int(input("Please enter the length of the time interval in minutes\n"))
+        WazirxAPI.generate_hourly_signals(symbol,scan_interval)
+    elif x == 7:
+        print("Please input the necessary information in the following order")
+        symbol = input("Please input symbol of crypto\n")
+        scan_interval = int(input("Please enter the length of the time interval in minutes\n"))
+        WazirxAPI.get_hourly_data(symbol, symbol, scan_interval)
+    return None
+
+def get_symbol_data():
+    val = ''
+    val = input("Please Press 1 for Crypto or Press 2 for Indian Stocks or write return to return to main menu\n")
+    if val.lower() == "return":
+        main_menu()
+    elif int(val) == 1:
+        symbol = input("Please input symbol\n")
+        WazirxAPI.write_hist_data(symbol, symbol)
+        g = WazirxAPI.read_data(symbol)
+        return g
+    elif int(val) == 2:
+        symbol = input("Please input symbol\n")
+        WazirxAPI.write_hist_data_stocks(symbol, symbol)
+        g = WazirxAPI.read_data(symbol)
+        return g
+    else:
+        print("Invalid input")
+        get_symbol_data()
+
+def signals_menu():
+    x = 0
+    y = 0
+    if y == 0:
+        y = get_symbol_data()
+    #symbol = input("Enter crypto symbol\n")
+    #WazirxAPI.write_hist_data(symbol, symbol)
+    #y = get_symbol_data()
+    print("Press 0 for returning to the main menu")
+    print("Press 1 for generating RSI signal for last traded price and time")
+    print("Press 2 for generating RSI signals for specified time period")
+    print("Press 3 for generating MACD signals for entire time period")
+    print("Press 4 for generating MACD signal of last traded price and time")
+    print("Press 5 for generating BB signals for entire time period")
+    print("Press 6 for generating BB signal of last traded price and time")
+    print("Press 7 for generating Stochastic signals for entire time period")
+    print("Press 8 for generating Stochastic signal of last traded price and time")
+    print("Press 9 for generating all signal values")
+    x = int(input("Press 11 for generating combined signal for the last traded price and time\n"))
+    if x == 0:
+        main_menu()
+    elif x == 1:
+        period = int(input("Please input the period for which the RSI needs to be calculated\n"))
+        print(WazirxAPI.Get_all_RSI_signals(y, period))
+        signals_menu()
+    elif x == 2:
+        f = int(input("Press 1 for RSI values, Press 2 for RSI signals or Press 3 for RSI plot\n"))
+        if f == 1:
+            period = 0
+            period = int(input("Please input the period for which the RSI needs to be calculated\n"))
+            j1 = RSI1.get_rsi(y,period)
+            print(j1[-30:-1])
+        elif f == 2:
+            period = 0
+            period = int(input("Please input the period for which the RSI needs to be calculated\n"))
+            y2 = RSI1.get_rsi(y, period)
+            j2 = RSI1.get_rsi_signal(y2)
+            print(j2[-30:-1])
+        elif f == 3:
+            period = 0
+            period = int(input("Please input the period for which the RSI needs to be calculated\n"))
+            y2 = RSI1.get_rsi(y, period)
+            y3 = RSI1.get_rsi_signal(y2)
+            RSI1.plot_rsi_buy_sell(y2,y3)
+        #print(WazirxAPI.Get_RSI_signals(y))
+        signals_menu()
+    elif x == 3:
+        f = 0
+        f = int(input("Press 1 for MACD signals or Press 2 for MACD plot\n"))
+        if f == 1:
+            print(WazirxAPI.get_MACD_signalline(y))
+        elif f == 2:
+            f1 = 0
+            f1 = int(input("Press 1 for MACD signals plot or Press 2 for MACD Crossover signals plot\n"))
+            y3 = MACD_OLD.get_macd_signal_line(y)
+            if f1 == 1:
+                y5 = MACD_OLD.get_signal_macd_signalline(y3)
+                print(y5[-30:-1])
+                MACD_OLD.plot_macd_buy_sell(y3,y5)
+            elif f1 == 2:
+                y4 = MACD_OLD.get_signal_macd_crossover(y3)
+                print(y4[-30:-1])
+                MACD_OLD.plot_macd_buy_sell(y3,y4)
+        signals_menu()
+    elif x ==4 :
+        print(WazirxAPI.get_MACD_signal(y))
+        signals_menu()
+    elif x == 5:
+        f3 = 0
+        f3 = int(input("Please 1 for bb signals or Press 2 for bb_plot of the signals for entire time period\n"))
+        if f3 == 1:
+            print(WazirxAPI.get_bb_signals(y))
+        elif f3 == 2:
+            g1 = BB_UTILS.get_bb_values(y)
+            g2 = BB_UTILS.get_bb_signal(g1)
+            BB_UTILS.plot_bb_buy_sell(g1,g2)
+        signals_menu()
+    elif x == 6:
+        print(WazirxAPI.get_bb_signal(y))
+        signals_menu()
+    elif x == 7:
+        WazirxAPI.get_Stochastic_signals(y)
+        signals_menu()
+    elif x ==8 :
+        print(WazirxAPI.get_Stochastic_signal(y))
+        signals_menu()
+    elif x == 11:
+        print(WazirxAPI.get_combined_signal(y))
+        signals_menu()
+    elif x == 9:
+        j1 = RSI1.get_rsi(y, 14)
+        print("RSI Values: ")
+        print(j1[-30:-1])
+        print(WazirxAPI.generate_combined_signals_values(y))
+        signals_menu()
+    else:
+        print("Wrong input, Please input a valid response")
+        signals_menu()
+
+    return None
+main_menu()
---
+++ templates/WazirxAPI.py
@@ -0,0 +1,292 @@
+import ccxt
+import time
+import yfinance as yf
+import myutils
+import indicators
+import numpy as np
+import pandas as pd
+
+exchange_id = 'wazirx'
+exchange_class = getattr(ccxt, exchange_id)
+exchange = exchange_class({
+    'apiKey': 'sbMNY7Udn3iwi6YrmOMcbfJDWd94hlSHB0HkY9zPMVBkeKDVtqF6sXLlodszAHJc',
+    'secret': 'O6gm86QkqnN61rxduqhR7pgHBSIZnaZtGxBTImx1',
+})
+def login():
+# from variable id
+    try:
+        exchange_id = 'wazirx'
+        exchange_class = getattr(ccxt, exchange_id)
+        exchange = exchange_class({
+            'apiKey': 'sbMNY7Udn3iwi6YrmOMcbfJDWd94hlSHB0HkY9zPMVBkeKDVtqF6sXLlodszAHJc',
+            'secret': 'O6gm86QkqnN61rxduqhR7pgHBSIZnaZtGxBTImx1',
+            })
+        if exchange==None:
+            print("Error with connection")
+        else:
+            return print('Login successful')
+
+    except:
+        exchange = None
+
+def Place_order(ticker,limit, buy, amount, price):
+    markets = exchange.createOrder(str(ticker), str(limit), str(buy), int(amount), int(price))
+    print(markets)
+    if markets == None:
+        print("Error with connection")
+    else:
+        return print('Order placed successfully')
+
+def get_current_prices_dir(ticker):
+    a = exchange.fetchTicker(str(ticker))
+    if a == None:
+        print("Error with connection")
+    else:
+        return a
+
+def get_current_price(ticker):
+    a = exchange.fetchTicker(str(ticker))
+    if a == None:
+        print("Error with connection")
+    else:
+        return a['ask']
+
+def write_hist_data(symbol, file_name):
+    data2 = yf.download(str(symbol) + '-INR', period= '90d', interval= '60m')
+    myutils.write_dataframe(file_name, data2)
+    #if data2 == None:
+    #    print('Error while adding hist data')
+    #else:
+    return print('Hist data has been written to main file')
+
+def write_hist_data_stocks(symbol, file_name):
+    data2 = yf.download(str(symbol) + '.NS', period= '90d', interval= '60m')
+    myutils.write_dataframe(file_name, data2)
+    #if data2 == None:
+    #    print('Error while adding hist data')
+    #else:
+    return print('Hist data has been written to main file')
+
+def read_data(filename):
+    filename1 = filename +'.csv'
+    data = myutils.read_dataframe(filename1)
+    return data
+
+
+def Get_RSI_signals(data, llimit=20, hlimit=80, period=14):
+    RSI = indicators.RSI(data,period)
+    if RSI > hlimit:
+        signal = -1
+    elif RSI < llimit:
+        signal = 1
+    else:
+        signal = 0
+    return RSI
+
+def Get_all_RSI_signals(data, llimit=20, hlimit=80, period=14):
+    RSI = indicators.RSI(data,period)
+    if RSI > hlimit:
+        signal = -1
+    elif RSI < llimit:
+        signal = 1
+    else:
+        signal = 0
+    return signal
+
+
+def Get_MACD_signals(data, short_window = 20, long_window = 50):
+    MACD = indicators.MACD(data, short_window, long_window)
+    return MACD
+
+def get_MACD_signalline(data, short_window = 20, long_window = 50, signal_window=12):
+    macd_data = data.copy()
+    # Create the set of short and long simple moving average, MACD, signal line over the
+    # respective periods
+    macd_data["SMA"] = macd_data['Close'].rolling(window = short_window, center=False).mean()
+    macd_data["LMA"] = macd_data['Close'].rolling(window = long_window, center=False).mean()
+    macd_data["MACD"] = macd_data['SMA'] - macd_data['LMA']
+    macd_data['signal_line'] = macd_data['MACD'].rolling(window = signal_window).mean()
+
+    #signals = pd.DataFrame(index = macd_data.index)
+    signals= macd_data.copy()
+    signals['buy_sell'] = 0.0
+    signals['buy_sell'][long_window:] = np.where((macd_data.MACD)[long_window:]
+                                            > 0, 1.0, 0.0)
+    signals['buy_sell'] = signals['buy_sell'].diff()
+    # return buy and sell signal
+    signals1 = pd.DataFrame(index=signals.index)
+    signals1 = signals
+    signals1['buy_sell'] = 0.0
+    signals1['buy_sell'][long_window:] = np.where((signals.MACD)[long_window:]
+                                            > signals.signal_line[long_window:], 1.0, 0.0)
+    signals1['buy_sell'] = signals1['buy_sell'].diff()
+    return signals1
+
+def plot_macd_buy_sell(macd_signal_line, signals, symbol):
+    """
+    Plot for macd price, shortma, longma, buy signal, sell signal
+    Input: data frame with all above information
+    Output: None
+    """
+    # putting all above together
+    fig = plt.figure(figsize=(15, 15))
+    plt.title(symbol)
+    # fig1
+    ax1 = fig.add_subplot(411, ylabel='Price in $')
+    macd_signal_line['price'].plot(ax=ax1, color='r', lw=2.)
+    macd_signal_line[['SMA', 'LMA']].plot(ax=ax1, lw=2.)
+    # fig2
+    ax2 = fig.add_subplot(412, ylabel='buy signal')
+    signals['price'].plot(ax=ax2, color='r', lw=2.)
+    ax2.plot(signals.loc[signals.buy_sell == 1.0].index, signals.price[signals.buy_sell == 1.0], '^', markersize=10,
+             color='m')
+    # fig3
+    ax3 = fig.add_subplot(413, ylabel='sell signal')
+    signals['price'].plot(ax=ax3, color='r', lw=2.)
+    ax3.plot(signals.loc[signals.buy_sell == -1.0].index, signals.price[signals.buy_sell == -1.0], 'v', markersize=10,
+             color='k')
+    # fig4
+    ax4 = fig.add_subplot(414, ylabel='buy sell signal')
+    signals['price'].plot(ax=ax4, color='r', lw=2.)
+    # add buy sell
+    ax4.plot(signals.loc[signals.buy_sell == 1.0].index, signals.price[signals.buy_sell == 1.0], '^', markersize=10,
+             color='g')
+    ax4.plot(signals.loc[signals.buy_sell == -1.0].index, signals.price[signals.buy_sell == -1.0], 'v', markersize=10,
+             color='k')
+    #
+    plt.show()
+
+def get_MACD_signal(data):
+    data1 = get_MACD_signalline(data)
+    return data1['buy_sell'].iloc[-1]
+
+def get_combined_signal(data):
+    x = Get_all_RSI_signals(data)
+    y1 = get_MACD_signalline(data)
+    y = get_MACD_signal(y1)
+    z = get_bb_signal(data)
+    xy = 0
+    if (x == 1 and y == 1) or (y == 1 and z == 1) or (x == 1 and z == 1):
+        xy = 1
+    elif (x == -1 and y == -1) or (y == -1 and z == -1) or (x == -1 and z == -1):
+        xy = -1
+    else:
+        pass
+    return f'Combined Signal: {xy}, RSI Signal: {x}, MACD signal: {y}, BB Signal: {z}'
+
+def get_bb_signals(data, period = 20, std = 2):
+    bb_data = pd.DataFrame(index=data.index)
+    bb_data['price'] = data['Close']
+    bb_data['Datetime'] = data['Datetime']
+    bb_data['middleband'] = bb_data['price'].rolling(window=period).mean()
+    bb_data['upperband'] = bb_data['middleband'] + std * (bb_data['price'].rolling(window=period).std())
+    bb_data['lowerband'] = bb_data['middleband'] - std * (bb_data['price'].rolling(window=period).std())
+    signals = pd.DataFrame(index=bb_data.index)
+    signals.head()
+    signals['Datetime'] = bb_data['Datetime']
+    signals['price'] = bb_data['price']
+    signals['upperband'] = bb_data['upperband']
+    signals['lowerband'] = bb_data['lowerband']
+    signals['sell']= 0.0
+    signals['buy']= 0.0
+    signals['buy'][period:] = np.where(bb_data['price'][period:] < bb_data['lowerband'][period:], -1.0, 0.0)
+    signals['sell'][period:] = np.where(bb_data['price'][period:] > bb_data['upperband'][period:],1.0,0)
+    signals['buy'] = signals['buy'].diff()
+    signals['sell'] = signals['sell'].diff()
+    signals.loc[signals['buy'] == -1.0,['buy']]=0
+    signals.loc[signals['sell'] == 1.0,['sell']]=0
+    signals['buy_sell'] = signals['buy'] + signals['sell']
+
+    return signals[['Datetime','price','upperband','lowerband', 'buy_sell']].iloc[-30:-1]
+
+def get_bb_signal(data, period = 20, std = 2):
+    x = get_bb_signals(data, period, std)
+    return x['buy_sell'].iloc[-1]
+
+def plot_bb_buy_sell(bb, signals):
+    """
+    Plot price, Bollinger band middle, lower, upper band with buy and sell signal
+    """
+    graph = plt.figure(figsize=(20,5))
+    ax1 = graph.add_subplot(111)
+    bb[['price','lowerband','upperband']].plot(ax = ax1,title ='Bollinger band Signals')
+    ax1.plot(signals.loc[signals.buy_sell == 1].index, signals.price[signals.buy_sell == 1], "^", markersize = 12, color = "g")
+    ax1.plot(signals.loc[signals.buy_sell == -1].index, signals.price[signals.buy_sell == -1], "v", markersize = 12, color = "m")
+    # plt.show()
+    plt.show()
+
+def get_Stochastic_signals(data, level=7):
+    data['H-14'] = data['High'].shift(14)
+    data['L-14'] = data['Low'].shift(14)
+    data['Stochastic value'] = (data['Close'] - data['L-14']) / (data['H-14'] - data['L-14'])
+    data['Slow Stochastic'] = data['Stochastic value'].rolling(3).mean()
+    data['price'] = data['Close'].copy()
+    data['S-Sigmoid'] = 1 / (1 + data['Slow Stochastic']) * 100
+    data.loc[data['Slow Stochastic'] > level, ['sell_signal']] = -1
+    data.loc[data['Slow Stochastic'] < level, ['sell_signal']] = 0.0
+    data.loc[data['Slow Stochastic'] < -level, ['buy_signal']] = 1
+    data.loc[data['Slow Stochastic'] > -level, ['buy_signal']] = 0
+    print(data[['Datetime','Close','Slow Stochastic']].iloc[-20:-1])
+    return data
+
+def get_Stochastic_signal(data, level=7):
+    data['H-14'] = data['High'].shift(14)
+    data['L-14'] = data['Low'].shift(14)
+    data['Stochastic value'] = (data['Close'] - data['L-14']) / (data['H-14'] - data['L-14'])
+    data['Slow Stochastic'] = data['Stochastic value'].rolling(3).mean()
+    data['price'] = data['Close'].copy()
+    data['S-Sigmoid'] = 1 / (1 + data['Slow Stochastic']) * 100
+    data.loc[data['Slow Stochastic'] > level, ['sell_signal']] = -1
+    data.loc[data['Slow Stochastic'] < level, ['sell_signal']] = 0.0
+    data.loc[data['Slow Stochastic'] < -level, ['buy_signal']] = 1
+    data.loc[data['Slow Stochastic'] > -level, ['buy_signal']] = 0
+    return f'Buy signal: {data.iloc[-1]["buy_signal"]} Sell signal: {data.iloc[-1]["sell_signal"]}'
+
+def get_hourly_data(symbol, file_name, scan_interval=10):
+    x = 0
+    while x == 0:
+        write_hist_data(symbol, file_name)
+        print(f'Scan complete, waiting for {scan_interval} mins for next Check')
+        time.sleep(scan_interval * 60)
+    return print("Scanned and saved")
+
+def generate_hourly_signals(symbol, scan_interval=10):
+    x= 0
+    while x == 0:
+        write_hist_data(symbol, symbol)
+        data = read_data(symbol)
+        print(get_combined_signal(data))
+        print(f'Scan complete, waiting for {scan_interval} mins for next Check')
+        time.sleep(scan_interval * 60)
+    return print("Scanned")
+
+def generate_combined_signals_values(data):
+    #y = Get_RSI_signals(data)
+    #print(y)
+
+    z = Get_MACD_signals(data)
+    print("BB Values:\n")
+    print(get_bb_signals(data))
+    print("Stochastic Values:\n")
+    z3 = get_Stochastic_signals(data)
+    print("MACD Values\n")
+    return z
+
+
+#data = get_current_price('FTM/INR')
+#print(data)
+#write_hist_data('MATIC', 'MATIC')
+#y = read_data('FTM')
+#print(Get_all_RSI_signals(y))
+#print(Get_MACD_signals(y))
+#x = get_MACD_signalline(y, )
+#print(x[x['buy_sell'] == 1])
+#print(get_MACD_signalline(y))
+#print(x['buy_sell'].iloc[-1])
+#print(get_MACD_signal(x))
+#print(get_combined_signal(y))
+#print(get_bb_signals(y))
+#print(get_bb_signal(y))
+#print(get_Stochastic_signals(y, 10))
+#get_hourly_data('FTM', 'FTM', 2)
+#print(generate_hourly_signals('FTM', 'FTM',y,2))
